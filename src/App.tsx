69bf43d872c4341299ba78661503b51ab3a3260a
hirokiwa <hirokifsys@gmail.com>
2023-01-22 00:15:51 +0900

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Top from './top/Top';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={`/`} element={<Top />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/top/Top.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Top = ():JSX.Element => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Top;
