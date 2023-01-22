2c5104e55c5d17006fbbd9483a6507d1c3de3fc9
hirokiwa <hirokifsys@gmail.com>
2023-01-23 02:16:41 +0900

    add growMasashi

--- src/top/Body.tsx
+++ src/top/Body.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { css } from 'styled-components'
+import masashi from '../../src/imgSource/masashi.png'
 
 const BodyOutline = styled.div`
     display:flex;
     background:#fae3d9;
     height: 100vh;
 `
+const MasashiDisplay = styled.div`
 
-const Body = ():JSX.Element => {
-  return (
-    <BodyOutline>
-        ぼくまさし
-    </BodyOutline>
-  );
+`
+const DataDisplay = styled.div`
+
+`
+
+const Body = (): JSX.Element => {
+    const [totalCarolie, setTotalCarolie] = useState(0);
+
+    const GrowMasashi = styled.img`
+        height:70%;
+        transform: scaleX(${String( 1 + totalCarolie/10000 )})
+    `
+    
+    return (
+        <BodyOutline>
+            <MasashiDisplay> 
+                <GrowMasashi src={masashi}></GrowMasashi>
+            </MasashiDisplay>
+            <DataDisplay>
+            </DataDisplay>
+        </BodyOutline>
+    );
 }
 
 export default Body;
